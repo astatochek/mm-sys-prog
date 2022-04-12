5dcfb6d4d55c915cecd1773f14004a066d8a08ce
astatochek <71458498+astatochek@users.noreply.github.com>
2022-04-12 18:05:04 +0300

    upd

--- JavaScript/11/11.7/script.js
+++ JavaScript/11/11.7/script.js
@@ -1,8 +1,59 @@
-let months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
-let days = ["Воскресение", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"]
+let par = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
 
-// day = [31, ]
-function next(day)
+function getList()
 {
+    let startDate = new Date()
+    startDate.setDate(document.getElementById("startDate").value)
+    startDate.setFullYear(document.getElementById("startYear").value)
+    let options = document.getElementById("startMonth").options
+    for (let i = 0; i < options.length; i++)
+    {
+        if (options[i].selected)
+        {
+            startDate.setMonth(i)
+        }
+    }
+    console.log(startDate)
 
+    let endDate = new Date()
+    endDate.setDate(document.getElementById("endDate").value)
+    endDate.setFullYear(document.getElementById("endYear").value)
+    options = document.getElementById("endMonth").options
+    for (let i = 0; i < options.length; i++)
+    {
+        if (options[i].selected)
+        {
+            endDate.setMonth(i)
+        }
+    }
+    console.log(endDate)
+
+    let chosenDay = 0
+    options = document.getElementById("dayOfWeek").options
+    for (let i = 0; i < options.length; i++)
+    {
+        if (options[i].selected)
+        {
+            chosenDay = (i + 8) % 7
+        }
+    }
+    console.log(chosenDay)
+
+    out = document.getElementById("output")
+    out.innerText = ``
+
+    num = (Number(endDate) - Number(startDate)) / (24 * 3600 * 1000)
+    let cur = startDate
+    for (let i = 0; i < num; i++)
+    {
+        cur.setDate(cur.getDate() + 1)
+        if (cur.getDay() == chosenDay)
+        {
+            out.innerText += `${cur.toLocaleString("ru", par)}\n`
+        }
+    }
 }
