b51e69ca2c269eaa558078aed88797370c4ce2c4
astatochek <71458498+astatochek@users.noreply.github.com>
2022-04-15 11:45:56 +0300

    upd

--- JavaScript/Индивидуальные задания/3/script.js
+++ JavaScript/Индивидуальные задания/3/script.js
@@ -6,6 +6,22 @@ let opts = {
     year: 'numeric'
 }
 
+function formatDate(num)
+{
+    let date = new Date(Number(num))
+    let dd = date.getDate();
+    if (dd < 10) dd = '0' + dd;
+  
+    let mm = date.getMonth() + 1;
+    if (mm < 10) mm = '0' + mm;
+  
+    let yy = date.getFullYear() % 100;
+    if (yy < 10) yy = '0' + yy;
+  
+    return dd + '.' + mm + '.' + yy;
+}
+
+
 function darken(num)
 {
     document.getElementById(`${num}`).style = "background-color: rgb(200, 200, 200);"
@@ -56,6 +72,7 @@ function putCol(ind, output, start, end, today) // создает колонки
                 cell.addEventListener("mouseover", () => darken(num))
             }
             cell.addEventListener("click", () => show(num))
+            cell.title = formatDate(num)
 
             div.append(cell)
         }
